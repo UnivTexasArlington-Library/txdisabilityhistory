f3142d27c2ba74ba95ccb7d5b537e5962d7c4aed
aleverenz <leverenz@uta.edu>
2016-06-01 09:12:12 -0500

    remove placeholder text from search form

--- sites/all/themes/disability/template.php
+++ sites/all/themes/disability/template.php
@@ -12,3 +12,11 @@ function disability_form_search_block_form_alter(&$form, &$form_state, $form_id)
 	$form['search_block_form']['#attributes']['role'] = 'search';
    
 } 
+
+// remove placeholder text from search form because we have Google search text
+function disability_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    // HTML5 placeholder attribute
+    $form['search_block_form']['#attributes']['placeholder'] = t('');
+  }
+}
