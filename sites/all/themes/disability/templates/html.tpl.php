5326c9bc4c8eaf466e97412c2f4d188201ef7b50
Krystal Schenk <kschenk@uta.edu>
2016-06-08 16:52:11 -0500

    Skip to main-content
    for all pages

--- sites/all/themes/disability/templates/html.tpl.php
+++ sites/all/themes/disability/templates/html.tpl.php
@@ -13,7 +13,7 @@
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
-    <a href="#herocaption" class="element-invisible element-focusable"><?php print t('Skip navigation'); ?></a>
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip navigation'); ?></a>
   </div>
   <?php print $page_top; ?>
   <?php print $page; ?>
