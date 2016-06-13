2fe6353edf9d1d04b9442504ac36f12693404020
Krystal Schenk <kschenk@uta.edu>
2016-06-13 13:33:02 -0500

    cancel documents page template
    still work to be done here.

--- sites/all/themes/disability/template.php
+++ sites/all/themes/disability/template.php
@@ -13,9 +13,9 @@ function disability_form_search_block_form_alter(&$form, &$form_state, $form_id)
    
 } 
 function <strong>disability</strong>_preprocess_page(&$variables) {
-  if (isset($variables['node']->documents)) {
+  if (isset($variables['node']->type)) {
     // If the content type's machine name is "my_machine_name" the file
     // name will be "page--my-machine-name.tpl.php".
-    $variables['theme_hook_suggestions'][] = 'page__documents' . $variables['node']->documents;
+    $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->documents;
   }
 }
