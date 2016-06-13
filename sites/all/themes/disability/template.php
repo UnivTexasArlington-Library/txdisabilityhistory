63a12c06809ef1b7d9d3256d057ee14e310fe861
Krystal Schenk <kschenk@uta.edu>
2016-06-13 13:29:30 -0500

    more header css

--- sites/all/themes/disability/template.php
+++ sites/all/themes/disability/template.php
@@ -12,3 +12,10 @@ function disability_form_search_block_form_alter(&$form, &$form_state, $form_id)
 	$form['search_block_form']['#attributes']['role'] = 'search';
    
 } 
+function <strong>disability</strong>_preprocess_page(&$variables) {
+  if (isset($variables['node']->type)) {
+    // If the content type's machine name is "my_machine_name" the file
+    // name will be "page--my-machine-name.tpl.php".
+    $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
+  }
+}
