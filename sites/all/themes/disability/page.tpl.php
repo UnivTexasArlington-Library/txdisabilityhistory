28f6ce2f4e83058f50193667d6191b833a25beb3
Krystal Schenk <kschenk@uta.edu>
2016-06-13 08:26:07 -0500

    update internal pages with container/header

--- sites/all/themes/disability/page.tpl.php
+++ sites/all/themes/disability/page.tpl.php
@@ -1,63 +1,65 @@
-<div class="row logo-wrapper-inside">
-  <div class="col-xs-12 col-sm-6 col-md-5 col-lg-4" id="logo">
-    <div class="hidden-print"> 
-      <a href="http://www.uta.edu/uta/">
-        <img alt="University of Texas at Arlington"  id="uta_logo" src="/txdisabilityhistory/sites/default/files/UTA-LOGO_GREEN.png" />
-      </a> 
-      <a href="/">
-        <img alt="UTA Libraries" id="lib_logo" src="/txdisabilityhistory/sites/default/files/UTA-Libraries_green.png"  />
-      </a>
-    </div>
-  </div>
-  <div class="col-xs-12 col-sm-5 col-md-6 col-lg-7" id="green-menu">
-    <nav class="navbar navbar-right">
-      <?php if (!empty($secondary_nav)):?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-    </nav>
-  </div>
-</div>
-<div class="row navbar-wrapper-inside">
-  <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-    <div class="<?php print $container_class; ?>">
-      <div class="navbar-header">
+<div class="container-fluid">
+  
+  <section class="row logo-wrapper">
+    
+    <div id="logo" class="col-xs-12 col-sm-6 col-md-5 col-lg-4" >
+      <?php if (!empty($page['logo'])): ?>
+      <?php print render($page['logo']); ?> 
+      <?php endif; ?> 
+    </div> <!-- end #logo -->
+    
+    <div id="green-menu" class="col-xs-12 col-sm-5 col-md-6 col-lg-7" >
+      <nav class="navbar navbar-right">
+        <?php if (!empty($secondary_nav)): ?>
+        <?php print render($secondary_nav); ?>
+        <?php endif; ?>
+      </nav>
+    </div> <!-- end #green-menu -->
+  </section> <!-- end.row logo-wrapper-->
+
+  <section class="row navbar-wrapper">
+    <header id="navbar-front" role="banner" class="<?php print $navbar_classes; ?>">
+
+      <div class="navbar-header col-xs-12 col-sm-6 col-md-5 col-lg-7">
         <?php if (!empty($site_name)): ?>
-          <h1><a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a></h1>
+          <h1><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a></h1>
         <?php endif; ?>
-        
+      </div> <!-- end.navbar-header-->
+      <div class="search-browse col-xs-12 col-sm-5 col-md-6 col-lg-4">
+        <!-- nav-mobile -->
         <?php if (!empty($primary_nav) || !empty($page['navigation'])): ?>
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
         <?php endif; ?>
-      </div>
+        <!-- nav-desktop -->
+        <?php if (!empty($primary_nav) || !empty($page['navigation'])): ?>
+          <div class="navbar-collapse collapse">
+            <nav role="navigation">
+              <?php if (!empty($primary_nav)): ?>
+              <div>
+                <?php print render($primary_nav); ?>
+              </div>
+              <?php endif; ?>
+              
+              <?php if (!empty($page['navigation'])): ?>
+              <div>
+                <?php print render($page['navigation']); ?>
+              </div>
+              <?php endif; ?>
+            </nav>
+          </div>
+        <?php endif; ?>
+      </div> <!-- end.search-browse -->
 
-      <?php if (!empty($primary_nav) || !empty($page['navigation'])): ?>
-        <div class="navbar-collapse collapse">
-          <nav role="navigation">
-            <?php if (!empty($primary_nav)): ?>
-            <div>
-              <?php print render($primary_nav); ?>
-            </div>
-            <?php endif; ?>
-            
-            <?php if (!empty($page['navigation'])): ?>
-            <div class="pull-right">
-              <?php print render($page['navigation']); ?>
-            </div>
-            <?php endif; ?>
-          </nav>
-        </div>
-      <?php endif; ?>
-    </div>
-  </header>
-</div> 
+    </header>
+  </section> <!-- end.navbar-wrapper" -->
+</div> <!-- end.container-fluid" -->
 
-
-<div class="main-container <?php print $container_class; ?>" role="main">
+<div class="main-container container" role="main">
 
   <div role="banner" id="page-header">
     <?php print render($page['header']); ?> 
