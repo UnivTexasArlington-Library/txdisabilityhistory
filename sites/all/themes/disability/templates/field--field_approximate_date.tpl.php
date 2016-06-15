593da960ef45389cd110f7d5c3f3c4a079274957
Krystal Schenk <kschenk@uta.edu>
2016-06-15 14:35:20 -0500

    approximate_date field template
    remove class "field-items"

--- sites/all/themes/disability/templates/field--field_approximate_date.tpl.php
+++ sites/all/themes/disability/templates/field--field_approximate_date.tpl.php
@@ -6,7 +6,7 @@
     
   <?php endif; ?>
   
-  <div class="field-items"<?php print $content_attributes; ?>>
+  <div <?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
       <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
     <?php endforeach; ?>
