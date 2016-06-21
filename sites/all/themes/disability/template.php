43b81a54acfc4ef36e95400356a1cdf57a6cceb3
Krystal Schenk <kschenk@uta.edu>
2016-06-21 09:50:09 -0500

    placeholder search #5

--- sites/all/themes/disability/template.php
+++ sites/all/themes/disability/template.php
@@ -7,9 +7,8 @@
 /**
 * hook_form_FORM_ID_alter
 */
-function disability_form_search_block_form_alter(&$form, &$form_state, $form_id) {
-    // Add ARIA role 'search' to the form
-	$form['search_block_form']['#attributes']['role'] = 'search';
-	// Alternative (HTML5) placeholder attribute instead of using the javascript
-    $form['views_exposed_form_search_page']['#attributes']['placeholder'] = t('Search');   
-} 
+function disability_form_views_exposed_form_alter(&$form, &$form_state, $form_id) {
+ if (isset($form['search_api_multi_fulltext'])) {
+    $form['search_api_multi_fulltext']['#attributes']['placeholder'] = t('search');
+  }
+}
