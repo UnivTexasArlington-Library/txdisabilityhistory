62234820ae41b55a6b6b9ce9d2bb0ebb1e41ac7a
Krystal Schenk <kschenk@uta.edu>
2016-06-21 09:53:30 -0500

    add aria role to search box

--- sites/all/themes/disability/template.php
+++ sites/all/themes/disability/template.php
@@ -9,6 +9,10 @@
 */
 function disability_form_views_exposed_form_alter(&$form, &$form_state, $form_id) {
  if (isset($form['search_api_multi_fulltext'])) {
+ 	// Add ARIA role 'search' to the form
+	$form['search_api_multi_fulltext']['#attributes']['role'] = 'search';
+	// Alternative (HTML5) placeholder attribute instead of using the javascript
     $form['search_api_multi_fulltext']['#attributes']['placeholder'] = t('search');
+
   }
 }
