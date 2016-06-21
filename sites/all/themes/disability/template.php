6ace3e85475e32495b7386ad607de4ef9f0b699c
Krystal Schenk <kschenk@uta.edu>
2016-06-21 09:39:48 -0500

    placeholder searchbox #3

--- sites/all/themes/disability/template.php
+++ sites/all/themes/disability/template.php
@@ -11,5 +11,5 @@ function disability_form_search_block_form_alter(&$form, &$form_state, $form_id)
     // Add ARIA role 'search' to the form
 	$form['search_block_form']['#attributes']['role'] = 'search';
 	// Alternative (HTML5) placeholder attribute instead of using the javascript
-    $form['search_block_form']['#attributes']['placeholder'] = t('Search');   
+    $form['edit_search_api_multi_fulltext']['#attributes']['placeholder'] = t('Search');   
 } 
