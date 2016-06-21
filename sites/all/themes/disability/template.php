d4104ba009088857ebbd9778220bbfd6b5cc5535
Krystal Schenk <kschenk@uta.edu>
2016-06-21 09:35:11 -0500

    Placeholder searchbox #2

--- sites/all/themes/disability/template.php
+++ sites/all/themes/disability/template.php
@@ -10,5 +10,6 @@
 function disability_form_search_block_form_alter(&$form, &$form_state, $form_id) {
     // Add ARIA role 'search' to the form
 	$form['search_block_form']['#attributes']['role'] = 'search';
-   
+	// Alternative (HTML5) placeholder attribute instead of using the javascript
+    $form['search_block_form']['#attributes']['placeholder'] = t('Search');   
 } 
