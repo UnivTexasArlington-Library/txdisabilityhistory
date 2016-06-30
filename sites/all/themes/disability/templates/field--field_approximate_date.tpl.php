614da38224e02dd3a285b57857a2abccd3b2fea0
Krystal Schenk <kschenk@uta.edu>
2016-06-30 10:33:50 -0500

    alignment of approximately field

--- sites/all/themes/disability/templates/field--field_approximate_date.tpl.php
+++ sites/all/themes/disability/templates/field--field_approximate_date.tpl.php
@@ -1,14 +1,9 @@
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="row">
-  <?php if (!$label_hidden): ?>
     
-      <div class="field-label"<?php print $title_attributes; ?>><strong><?php print $label ?>:</strong></div>
-    
-  <?php endif; ?>
-  
   <div <?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
+      <div class="field-item col-sm-12 <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
     <?php endforeach; ?>
 
   </div>
