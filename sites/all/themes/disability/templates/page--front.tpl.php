1faa9c86ada746bbfd4eaf45f7618a058cfe2543
Krystal Schenk <kschenk@uta.edu>
2016-08-17 14:08:55 -0500

    Aria Label updates & minor CSS updates

--- sites/all/themes/disability/templates/page--front.tpl.php
+++ sites/all/themes/disability/templates/page--front.tpl.php
@@ -8,13 +8,13 @@
       <?php endif; ?> 
     </div> <!-- end #logo -->
     
-    <div id="green-menu" class="hidden-xs">
+    <div id="about-menu" class="hidden-xs">
       <nav class="navbar" role="navigation" aria-labelledby="about menu">
         <?php if (!empty($secondary_nav)): ?>
         <?php print render($secondary_nav); ?>
         <?php endif; ?>
       </nav>
-    </div> <!-- end #green-menu -->
+    </div> <!-- end #about-menu -->
   </section> <!-- end.row logo-wrapper-->
 
   <section class="row navbar-wrapper">
@@ -39,7 +39,7 @@
         <!-- nav-desktop -->
         <?php if (!empty($primary_nav) || !empty($page['navigation'])): ?>
           <div class="navbar-collapse collapse">
-            <nav role="navigation" aria-labelledby="main menu">
+            <nav id="main-menu" role="navigation" aria-labelledby="main menu">
               <?php if (!empty($secondary_nav)): ?>
               <div class="visible-xs">
                 <?php print render($secondary_nav); ?>
